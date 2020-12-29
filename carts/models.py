22a3d7e0131a0d03ccd4d8e78b1983adc5c0ff19
Jefter Targino <jeftertargino@alu.uern.br>
2020-12-28 23:35:41 -0300

    Atualização total carrinho

--- carts/models.py
+++ carts/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.db.models.signals import pre_save, post_save, m2m_changed
 
 from products.models import Product
 
@@ -9,7 +10,7 @@ class CartManager(models.Manager):
     def new_or_get(self, request):
         cart_id = request.session.get("cart_id", None)
         qs = self.get_queryset().filter(id = cart_id)
-        if qs.count == 1:
+        if qs.count() == 1:
             new_obj = False
             cart_obj = qs.first()
             if request.user.is_active and cart_obj.user is None:
@@ -31,6 +32,7 @@ class CartManager(models.Manager):
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null = True, blank = True)
     products = models.ManyToManyField(Product, blank = True)
+    subtotal = models.DecimalField(default = 0.00, max_digits=100, decimal_places = 2)
     total = models.DecimalField(default = 0.00, max_digits=100, decimal_places = 2)
     updated = models.DateTimeField(auto_now = True)
     timestamp = models.DateTimeField(auto_now_add = True)
@@ -39,3 +41,26 @@ class Cart(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+def m2m_changed_cart_receiver(sender, instance, action, *args, **kwargs):
+  #print(action)
+  if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
+    #print(instance.products.all())
+    #print(instance.total)
+    products = instance.products.all() 
+    total = 0 
+    for product in products: 
+      total += product.price 
+    if instance.subtotal != total:
+      instance.subtotal = total
+      instance.save()
+    #print(total) 
+    instance.subtotal = total
+    instance.save()
+
+m2m_changed.connect(m2m_changed_cart_receiver, sender = Cart.products.through)
+
+def pre_save_cart_receiver(sender, instance, *args, **kwargs):
+  instance.total = instance.subtotal + 10
+
+pre_save.connect(pre_save_cart_receiver, sender = Cart)
